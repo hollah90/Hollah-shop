fcc50439985208c36682c88e67464b7436c9a719
hollah90 <hollahtech@gmail.com>
2023-02-14 04:07:44 +0100

    sign in

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -32,7 +32,18 @@ const SignInForm = () => {
                console.log(response)
                resetFormFields();
 
-          } catch (error) { }
+          } catch (error) {
+               switch (error.code) {
+                    case 'auth/wrong-password':
+                         alert('incorrect password for email');
+                         break;
+                    case 'auth/user-not-found':
+                         alert('no user associated with this email');
+                         break;
+                    default:
+                         console.log(error);
+               }
+          }
      }
 
      const handleChange = (event) => {
@@ -68,7 +79,7 @@ const SignInForm = () => {
                          value={password} />
                     <div className="buttons-container">
                          <Button type="submit">Sign In</Button>
-                         <Button buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
+                         <Button type="button" buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
                     </div>
 
 
